0df46c7563dea46856289a02bcb9d7037631cb95
harikrishnanta <118351703+harikrishnanta@users.noreply.github.com>
2023-03-27 17:43:40 +0530

    Add files via upload
    FlipkartClone 27-3

---
+++ ImageSlider.js
@@ -0,0 +1,19 @@
+export const ImageSlider = [
+
+    {
+        img : "https://rukminim1.flixcart.com/fk-p-flap/1688/280/image/aa6eb203f1250eaf.jpg?q=50",
+        link : "https://www.flipkart.com/mobiles/~cs-dlb3fdywcd/pr?sid=tyy%2C4io&collection-tab-name=realme+C55&param=4034344&otracker=hp_bannerads_1_2.bannerAdCard.BANNERADS_Cat_Mob_HPW_Realme%2BC55_R9LVN3Y05LQE"
+    },
+    {
+        img : "https://rukminim1.flixcart.com/fk-p-flap/1688/280/image/5533318739d92360.jpg?q=50",
+        link : "https://www.flipkart.com/travel/flights?param=Travel-DTHPW-DOM999_25thMar&otracker=hp_bannerads_3_2.bannerAdCard.BANNERADS_DOM999_QPELFX6ECPQJ"
+    },
+    {
+        img : "https://rukminim1.flixcart.com/fk-p-flap/1688/280/image/112b0206a861accc.jpg?q=50",
+        link : "https://www.flipkart.com/travel/flights?param=Travel-DTHPW-DOM999_25thMar&otracker=hp_bannerads_3_2.bannerAdCard.BANNERADS_DOM999_QPELFX6ECPQJ"
+    },
+    {
+        img : "https://rukminim1.flixcart.com/flap/1688/280/image/75a15c3e19c3f7de.jpg?q=50",
+        link : "https://www.flipkart.com/furniture-furniturestudio-store?otracker=hp_bannerads_4_2.bannerAdCard.BANNERADS_Furniture%2BBestsellers_7Z3Q78W49QYC"
+    }
+]
---
+++ javascript.js
@@ -0,0 +1,60 @@
+import {ImageSlider} from "./ImageSlider.js"
+
+/*****Image Slider**********/
+let imageSliderListE1 = document.querySelector(".imageSliderList")
+let imageSliderListHTML = ''
+console.log(ImageSlider)
+
+ImageSlider.forEach(el =>{
+    imageSliderListHTML += `
+    <div class="imageSliderItem">
+        <a href="${el.link}">
+            <img src="${el.img}"/>
+        </a>
+    </div>
+    `
+})
+imageSliderListE1.innerHTML = imageSliderListHTML;
+
+// let imageSliderListE1AllItems = document.querySelectorAll(".imageSliderList")
+// console.log(imageSliderListE1AllItems)
+
+let prev_imageE1 = document.getElementById("prev_image")
+let next_imageE1 = document.getElementById("next_image")
+
+let start=0;
+let end=-300;
+
+prev_imageE1.addEventListener("click",handlePreveImage)
+next_imageE1.addEventListener("click",handleNextImage)
+
+function handlePreveImage(){
+    let imageallList = document.querySelectorAll(".imageSliderItem")
+    console.log(imageallList)
+    if(start < 0)
+    start+=100
+    imageallList.forEach(el=>{
+        el.style.transform = `translatex(${start}%)`
+    })
+}
+function handleNextImage(){
+    let imageallList = document.querySelectorAll(".imageSliderItem")
+    console.log(imageallList)
+    if(start > end)
+    start-=100
+    imageallList.forEach(el=>{
+        el.style.transform = `translatex(${start}%)`
+    })
+}
+
+function renderImageSlider(){
+    if(start > end){
+        handleNextImage()
+    }
+    else{
+        start = 100
+    }
+}
+
+setInterval(renderImageSlider,5000)
+
